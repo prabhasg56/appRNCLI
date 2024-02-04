8b3d735accdb655a18a7e39a6caff0fc04f388c7
PRABHAKAR MANDAL <86845434+prabhasg56@users.noreply.github.com>
2024-02-04 22:41:38 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,96 @@
+import 'react-native-gesture-handler';
+import {
+  NavigationContainer,
+  useNavigation,
+  DrawerActions,
+} from '@react-navigation/native';
+import HomeScreen from './src/Screens/HomeScreen';
+import ProfileScreen from './src/Screens/ProfileScreen';
+import UserScreen from './src/Screens/UserScreen';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/Entypo';
+import DrawerContent from './DrawerContent';
+// import SplashScreen from 'react-native-splash-screen';
+import {useEffect} from 'react';
+import LoginPage from './src/Screens/Login&Register/Login';
+import RegisterPage from './src/Screens/Login&Register/Register';
+
+const StackNav = () => {
+  const Stack = createNativeStackNavigator();
+  const navigation = useNavigation();
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        statusBarColor: '#0163d2',
+        headerStyle: {
+          backgroundColor: '#0163d2',
+        },
+        headerTintColor: '#fff',
+        headerTitleAlign: 'center',
+      }}>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerLeft: () => {
+            return (
+              <Icon
+                name="menu"
+                onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+                size={30}
+                color="#fff"
+              />
+            );
+          },
+        }}
+      />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen
+        name="User"
+        component={UserScreen}
+        options={{
+          headerShown: true,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const DrawerNav = () => {
+  const Drawer = createDrawerNavigator();
+  return (
+    <Drawer.Navigator
+      drawerContent={props => <DrawerContent {...props} />}
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Drawer.Screen name="Home" component={StackNav} />
+    </Drawer.Navigator>
+  );
+};
+function App() {
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     SplashScreen.hide();
+  //   }, 900);
+  // }, []);
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="Register" component={RegisterPage} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+
+        <Stack.Screen name="Drawer" component={DrawerNav} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
