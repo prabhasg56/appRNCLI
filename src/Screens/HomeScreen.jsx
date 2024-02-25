64e0b89ccbaa67e151db108b998570ee63661974
PRABHAKAR MANDAL <86845434+prabhasg56@users.noreply.github.com>
2024-02-25 18:36:40 +0530

    Infinite scroll

--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -1,25 +1,21 @@
 import {StyleSheet, Text, View, Button} from 'react-native';
+import MyList from '../components/MyList';
 
 function HomeScreen(props) {
   console.log(props);
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.headingStyle}>Login & Register In React Native</Text>
-      {/* <Text style={styles.textStyle}>This is Home Screen</Text> */}
-      {/* <Button
-        title="Profile"
-        onPress={() => props.navigation.navigate('Profile',{
-          name:"Aadarsh"
-        })}
-      /> */}
+      {/* <Text style={styles.headingStyle}>Login & Register In React Native</Text> */}
+      <Text style={styles.textStyle}>This is Home Screen</Text>
+      <MyList/>
     </View>
   );
 }
 const styles = StyleSheet.create({
   viewStyle: {
     display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     flex: 1,
   },
   textStyle: {
